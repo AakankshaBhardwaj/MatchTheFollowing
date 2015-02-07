e5e377fc4dbf37c6fb91acf024b51d576dcadb64
Aakanksha <aakanksha@headerlabs.com>
2015-02-07 15:18:47 +0530

    final commit

--- match.js
+++ match.js
@@ -3,81 +3,89 @@
  */
 function allowDrop(event,ui) {
     event.preventDefault();
-     var u=event;
+     //var u=event;
 }
 var b,c;
 var data;
 var count=0;
 function drop(event) {
     event.preventDefault();
-    data = event.dataTransfer.getData("Text");
+    data = event.dataTransfer.getData("Text");//dragged element
     event.target.appendChild(document.getElementById(data));
-    //document.getElementById("demo").innerHTML = "The p element was dropped";
     b = data;
     // document.getElementById('footer').innerHTML=data.text();
     $('#' + event.target.id).hide('explode');
     c = event.target.id
-    alert(c);
+   // alert(c);  // the value of destination
 
-    $('#footer').append($('#footer').text($('#' + event.target.id).text()))
+    $('#footer').append($('#footer').text($('#' + event.target.id).text()));
     $('#footer').slideDown(500);
-    count++;
 
-    alert("the value of b:" + b);
+    /*createItem();
 
+    function createItem()
 
-}
-
-var sourceId=b;
-var targetID=c;
-
-
-
-
-function result() {
-
-
-
-
-    if(sourceId.charAt(sourceId.length-1 )== targetID.charAt(targetID.length-1))
     {
-        count+=1;
-        score();
+       //alert("create function is called");
+        //alert (b);alert(c);
+       // var val= $('#'+b).html(); //extract the text of the source
+        var valL=$('#'+c).text(); // extract the text of target and source
+       // alert(valS);
+        alert(valL);  // shows left+right
+        //var newItem=  '<>'+ "+valLast; // make a new item  to be added
+        //alert(newItem);
+        //if(b.charAt(sourceID.length-1 )== lastID.charAt(lastID.length-1)){score+=1;}
+        $('#footer').append('<div id= '+addID+'>'+ " "+"valL  " + '</div>');
+         $('#footer').text(valL);
 
+    }
+*/
+  result();
 
+    //alert("the value of b:" + b);
 
+    function result() {
 
+        if (b.charAt((b.length) - 1) == c.charAt((c.length) - 1)) {
+            // alert("true");
+            count++;
+            //alert("your score is" + count);
+
+            score(count);
+
+        }
+        else
+            score(count);     //alert("you have " + count + "perfect match");
 
     }
 
 
 
 }
-function score() {
-
-    alert("your score is" + count);
-
-
-
-
-}
-
-
-
-
-
-
     function dragStart(event) {
         event.dataTransfer.setData("Text", event.target.id);
 
         //var c=$('#'+event.target(b));
-
-
-        $('#footer').text($('#' + event.target.id).text());
+        // $('#footer').text($('#' + event.target.id).text());
         //uid.drop.get(uid);
-
+//The dataTransfer object is similar to the clipboardData object, but it can be used for customizing drag-and-drop operations.
+        //The dataTransfer object is only accessible through the event object in case of the following events: ondrag, ondragend, ondragenter, ondragleave, ondragover, ondragstart and the ondrop.
 
     }
+function score( count){
+    $('#score').text("Your score is "+count);
+
+}
+
+
+function reset()
+{
+    location.reload();
+
+}
+
+
+
 
 
 
@@ -91,26 +99,27 @@ $(document).ready (function(){
     <!-- left division -->
     $('#container_left').append('<div id ="container_right" >right container</div>')
 
-    $('#container_left1').html('<div id ="left_1" class ="left_1" draggable="true"  ondrop="drop(event)" ondragstart="dragStart(event)" ondrag="dragging(event)" ondragover="allowDrop(event,ui)"  >MULTIPLE INHERITANCE </div>')
+    $('#container_left1').html('<div id ="left_1" class ="left_1" draggable="true"  droppable="true" ondrop="drop(event)" ondragstart="dragStart(event)" ondrag="dragging(event)" ondragover="allowDrop(event)"  >MULTIPLE INHERITANCE </div>')
 
-    $('#container_left1').append('<div id ="left_2"  class="left_1" draggable="true"  ondrop="drop(event)" ondragover="allowDrop(event)" ondragstart="dragStart(event)" ondrag="dragging(event)" >KERNEL</div>')
-
-
-    $('#container_left1').append('<div id ="left_3" class="left_1" draggable="true"  ondrop="drop(event)" ondragover="allowDrop(event)" ondragstart="dragStart(event)" ondrag="dragging(event)" containment =#right_2" snap="#right_2" snapMode = "both" snapTolerance ="30 " >SUPER KEYWORD</div>')
-
-    $('#container_right').html('<div id ="right_1"  draggable="true" ondrop="drop(event)" ondragover="allowDrop(event)" ondragstart="dragStart(event)" ondrag="dragging(event)" >OPERATING SYSTEM</div>')
-
-    $('#container_right').append('<div id ="right_2" draggable="true" ondrop="drop(event)" ondragover="allowDrop(event)" ondragstart="dragStart(event)" ondrag="dragging(event)" >JAVA</div>')
-
-    $('#container_right').append('<div id ="right_3"   revert="true" droppable="true" draggable="true" ondrop="drop(event)" ondragover="allowDrop(event)" ondragstart="dragStart(event)" ondrag="dragging(event)";tolerance="fit" >C++</div>')
+    $('#container_left1').append('<div id ="left_2"  class="left_1" draggable="true" droppable="true" ondrop="drop(event)" ondragover="allowDrop(event)" ondragstart="dragStart(event)" ondrag="dragging(event)" >KERNEL</div>')
 
 
+    $('#container_left1').append('<div id ="left_3" class="left_1" draggable="true"  droppable="true" ondrop="drop(event)" ondragover="allowDrop(event)" ondragstart="dragStart(event)" ondrag="dragging(event)" containment =#right_2" snap="#right_2" snapMode = "both" snapTolerance ="30 " >SUPER KEYWORD</div>')
+
+    $('#container_right').html('<div id ="right_1"  draggable="true"  droppable="true" ondrop="drop(event)" ondragover="allowDrop(event)" ondragstart="dragStart(event)" ondrag="dragging(event)" >C++</div>')
+
+    $('#container_right').append('<div id ="right_2" draggable="true"  droppable="true"ondrop="drop(event)" ondragover="allowDrop(event)" ondragstart="dragStart(event)" ondrag="dragging(event)" >OPERATING SYSTEM</div>')
+
+    $('#container_right').append('<div id ="right_3"  droppable="true" draggable="true" ondrop="drop(event)" ondragover="allowDrop(event)" ondragstart="dragStart(event)" ondrag="dragging(event)";tolerance="fit" >JAVA</div>')
 
     $('#main_con').append('<div id="footer"  > footer</div>')
-    $('#main_con').append(' <button type="button" id="score" onclick="result()" >SCORE</button>' )
-    $('#main_con').append(' <button type="reset"  id="reset" onclick="resetform()" >RESET</button>' )
+
+    $('#main_con').append(' <button type="button" id="score"  >SCORE</button>' )
+
+    $('#main_con').append(' <button type="reset"  id="reset" onclick="reset()" >RESET</button>' )
 
     $('#footer').hide();
 });
+
 
 
